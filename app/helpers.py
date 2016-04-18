8a722e3017f5272c6e3a428979c9db7a140c2859
PeterCP <pacp95@gmail.com>
2016-04-18 14:45:10 -0500

    Refactored. To be implemented: Handle persistent sessions.

--- app/helpers.py
+++ app/helpers.py
@@ -1,16 +1,18 @@
-from flask import flash, session
+from flask import session
 
-from app import app
-from models import User
+from app.models import User
 
 
 def current_user():
 	user_id = session.get('user_id', None)
 	if user_id:
 		user = User.query.get(user_id)
-		return user
-	else:
-		return None
+		if user:
+			return user
+	return None
+
+def current_user_id():
+	user_id = session.get('user_id', None)
 
 def login(user):
 	session['user_id'] = user.id
