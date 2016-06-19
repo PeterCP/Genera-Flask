42706562838227b27567ec9974a34435d4c9e626
PeterCP <pacp95@gmail.com>
2016-06-19 14:34:06 -0500

    Added Flask-Migrate and Flask-Script to better manage the application.

--- genera.py
+++ genera.py
@@ -1,12 +1,12 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
 from app import app
+from app.models import db
 
-@app.cli.command('initdb')
-def initdb():
-	from app import models
-	models.db.create_all()
-	print 'Database initialized successfully!'
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
-@app.cli.command('run')
-def run():
-	app.run()
-
+if __name__ == '__main__':
+	manager.run()
